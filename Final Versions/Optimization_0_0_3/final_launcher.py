5940c725d1aacaf6b6d3b1ce336420d72a8bd722
Ludovico Leardi <95071605+ludoleardi@users.noreply.github.com>
2023-04-03 21:36:20 +0200

    Upload opti 003 & 004

---
+++ Final Versions/Optimization_0_0_3/final_launcher.py
@@ -0,0 +1,41 @@
+"""
+This program is used to choose the control mode. If the user doesn't move the head in 5 seconds the automatic mode will be used.
+Otherwise, if the user moves the neck, the MUSE control mode will be used.
+"""
+
+import time
+
+#Muse2 custom module
+import MuseControl as mc
+
+#Main codes
+import final_muse
+import final_auto
+
+if __name__ == '__main__':
+    theta, gamma, alpha = mc.museDxSx() #get gyro data from muse
+
+    startTime = time.time()
+    currentTime = time.time()
+    moved = False
+
+    while currentTime - startTime <= 5000 and moved is False: #5 seconds have passed or the user moved the head
+        if gamma > mc.SX_GAMMA or gamma < mc.DX_GAMMA:
+            moved = True
+        
+        if prec > mc.DX_THETA and prec < mc.SX_THETA:
+            if theta > mc.SX_THETA or theta < mc.DX_THETA:
+                moved = True
+        prec = theta
+        
+        #print(alpha)     #FUNZIONA!!!
+        if alpha > mc.FW_ALPHA or alpha < mc.RW_ALPHA:
+            moved = True
+
+    if moved is True: #The user is able to move his neck
+        print('Launching control mode...')
+        final_muse.main()
+
+    else:
+        print('Launching auto mode...')
+        final_auto.main()
